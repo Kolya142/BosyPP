cb8c0af7372d8bafc7ccb6d6a32b842ad02e2c15
Kolya142 <87275865+Kolya142@users.noreply.github.com>
2025-04-23 09:49:00 +0300

    enum & struct

---
+++ test.bpp.js
@@ -0,0 +1,5 @@
+// Enum (Color)
+const Color = {RED: 0,0: "RED",GREEN: 1,1: "GREEN",BLUE: 2,2: "BLUE",BLACK: 3,3: "BLACK",GRAY: 4,4: "GRAY",};console.log(console.log);
+if (((Color.RED)) === (Color.RED)) {{console.log("RED is RED");
+}}if (((Color.RED)) === (Color.GREEN)) {{console.log("RED is GREEN");
+}}function Vec2(_x, _y) { return {x: _x, y: _y}; }
