3d17317398c844533964b82f8c4225a12d87a3d6
Kolya142 <87275865+Kolya142@users.noreply.github.com>
2025-04-25 08:26:12 +0300

    fsm demo && match default

--- examples/test.bpp.js
+++ examples/test.bpp.js
@@ -1,7 +1,7 @@
 const Graphics = {// Enum (Color)
 Color: {RED: 0,0: "RED",GREEN: 1,1: "GREEN",BLUE: 2,2: "BLUE",BLACK: 3,3: "BLACK",GRAY: 4,4: "GRAY",},PrintColor: function (col){console.log(Graphics.Color[col]);
 }};Graphics.PrintColor(Graphics.Color.BLUE);
-if (((Graphics.Color.RED)) === (Graphics.Color.RED)) {{console.log("RED is RED");
-}}if (((Graphics.Color.RED)) === (Graphics.Color.GREEN)) {{console.log("RED is GREEN");
+if (0) {}else if (((Graphics.Color.RED)) === (Graphics.Color.RED)) {{console.log("RED is RED");
+}}else if (((Graphics.Color.RED)) === (Graphics.Color.GREEN)) {{console.log("RED is GREEN");
 }}function Vec2(_x, _y) { return {x: _x, y: _y}; }const my_vec=Vec2(5,3);
 console.log(my_vec)
--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,5 @@
 cp bosypp.bpp.js bosypp.bpp.js.1
 node bosypp.bpp.js bosypp.bpp
-node bosypp.bpp.js bosypp.bpp || cp bosypp.bpp.js.1 bosypp.bpp.js
+# cp bosypp.bpp.js bosypp.bpp.js.2
+node bosypp.bpp.js examples/test.bpp || cp bosypp.bpp.js.1 bosypp.bpp.js
 rm bosypp.bpp.js.1
