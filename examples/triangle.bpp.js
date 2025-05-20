ec32374488b5708767543e2a294207df590b3816
Kolya142 <87275865+Kolya142@users.noreply.github.com>
2025-05-20 20:55:53 +0300

    some fixes

--- examples/triangle.bpp.js
+++ examples/triangle.bpp.js
@@ -1,8 +1,9 @@
-function main(){const buffer=Array(64*64);
-for(let i=0;
-i<64;
-++i){for(let j=0;
-j<64;
-++j){buffer[i+(64-j)*64]=(0===(j&i))?"#":" ";
-}}console.log(buffer.join(""));
+function main() {const buffer  = Array(50*50+50);
+for(let i  = 0;
+i<50;
+++i){for(let j  = 0;
+j<50;
+++j){buffer[i+(50-1-j)*(50+1)]=(0===(j&i))?"#":" ";
+}buffer[i*(50+1)+50]="\n";
+}console.log(buffer.join(""));
 }main();
--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,5 @@
 cp bosypp.bpp.js bosypp.bpp.js.1
 node bosypp.bpp.js bosypp.bpp
+cp bosypp.bpp.js /tmp/bpp.bpp.js
 node bosypp.bpp.js bosypp.bpp || cp bosypp.bpp.js.1 bosypp.bpp.js
 rm bosypp.bpp.js.1
